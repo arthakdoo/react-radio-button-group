55dc433da3b4cfc4e017a6ee7f74e6eec92e19e1
Darko Maksimovic <dmaksimovic@gogoair.com>
2016-12-30 14:26:43 +0100

    Unique ids, CSS class identifiers, fireOnMount

--- src/index.js
+++ src/index.js
@@ -2,15 +2,25 @@ import React from 'react';
 
 const isString = (obj) => typeof obj === "string";
 
+const ComponentName = 'react-radio-button-group';
+const getUniqueId = seed => ComponentName + '-' + seed;
+
 class ReactRadioButtonGroup extends React.Component {
     constructor(props) {
         super(props);
         this.state = {currentValue: this.props.defaultValue};
         this.handleChange = this.handleChange.bind(this);
+        this.fireCurrentValue = this.fireCurrentValue.bind(this);
+    }
+
+    fireCurrentValue() {
+        if (this.props.onChange) {
+            this.props.onChange(this.state.currentValue);
+        }
     }
 
     handleChange(event) {
-        const newValue = event.target.id;
+        const newValue = event.target.name;
 
         if (newValue === this.state.currentValue) {
             return;
@@ -20,32 +30,38 @@ class ReactRadioButtonGroup extends React.Component {
             currentValue: newValue
         });
 
-        if (this.props.onChange) {
-            this.props.onChange(newValue);
+        this.fireCurrentValue();
+    }
+
+    componentDidMount() {
+        if (this.props.fireOnMount) {
+            this.fireCurrentValue();
         }
     }
 
     render() {
+        const groupClassName = this.props.groupClassName || '';
+        const itemClassName = this.props.itemClassName || '';
         return (
-            <span>
+            <span className={groupClassName}>
                 {this.props.options.map(option => {
                     const value = isString(option) ? option : option.value;
                     const label = isString(option) ? option : option.label;
+                    const inputClassName = option.inputClassName || this.props.inputClassName || '';
+                    const labelClassName = option.labelClassName || this.props.labelClassName || '';
+                    const inputId = getUniqueId(value);
                     const isChecked = this.state.currentValue === value;
 
                     return (
-                        <span key={value}>
+                        <span key={value} className={itemClassName}>
                             <input
                                 type="radio"
                                 checked={isChecked}
-                                id={value}
+                                id={inputId}
                                 onChange={this.handleChange}
-                                className={this.props.inputClassName}
-                            />
-                            <label
-                                htmlFor={value}
-                                className={this.props.labelClassName}
-                            >
+                                className={inputClassName}
+                                name={value}/>
+                            <label htmlFor={inputId} className={labelClassName}>
                                 {label}
                             </label>
                         </span>
@@ -61,7 +77,9 @@ ReactRadioButtonGroup.propTypes = {
         React.PropTypes.oneOfType([
             React.PropTypes.shape({
                 value: React.PropTypes.string,
-                label: React.PropTypes.string
+                label: React.PropTypes.string,
+                inputClassName: React.PropTypes.string,
+                labelClassName: React.PropTypes.string
             }),
             React.PropTypes.string
         ])
@@ -69,7 +87,10 @@ ReactRadioButtonGroup.propTypes = {
     defaultValue: React.PropTypes.string.isRequired,
     onChange: React.PropTypes.func,
     inputClassName: React.PropTypes.string,
-    labelClassName:  React.PropTypes.string
+    labelClassName:  React.PropTypes.string,
+    itemClassName: React.PropTypes.string,
+    groupClassName: React.PropTypes.string,
+    fireOnMount: React.PropTypes.bool
 };
 
 export default ReactRadioButtonGroup;
